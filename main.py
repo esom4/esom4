0666aba56ec4425b53973914e3fc7aa319c18176
stefanosansone <stefanosansone.dev@gmail.com>
2023-04-22 17:00:21 +0200

    fix file name

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 from databases import franklin_genoox
 
 # TODO : Add functions in franklin_genoox and use "from databases import franklin_genoox" instead of this
-exec(open('file.py').read())
+exec(open('databases/franklin_genoox.py').read())
