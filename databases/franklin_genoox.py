50b4ca7d5daf0b5f1cf7ac09d321901b5e54e095
nicholasmagatti <nicholas.magatti@hotmail.it>
2023-05-06 17:11:45 +0200

    Add dynamic filters
    The user can now select the desired filters.
    
    Currently, if the execution stops and then the user run again, they have to insert the same filters as before, otherwise the result will not be consistent.

--- databases/franklin_genoox.py
+++ databases/franklin_genoox.py
@@ -14,10 +14,58 @@ import configparser
 import time
 import pandas as pd
 import os
+from typing import List
 
 STRING_RESULT = '_results'
 EXCEL_FILE_EXTENSION = '.xlsx'
 FRANKLIN_CLASSIFICATION = 'Franklin Classification'
+
+
+def printReceivedInput(elaboratedInput) -> None:
+    print('Input received:')
+    if elaboratedInput == []:
+        print('Nothing selected')
+    else:
+        print(elaboratedInput)
+    print()  # leave a line of space before the next print
+
+
+def getUserInput() -> str:
+    userInput = input("Write here:")
+    return userInput
+
+
+def getUserInputList() -> List[str]:
+    userInput = getUserInput()
+    if userInput == '':
+        return []
+    else:
+        return userInput.split(",")
+
+
+def getUserInputListFromNumberedList(elementList: List[str]) -> List[str]:
+    # the user will insert the selected elements as 1,2,3 as the position in the option list starting from 1 (e.g. 1 is elementList[0])
+    userInput = getUserInputList()
+    if len(userInput) == 0:  # if input is empty
+        return userInput
+    # otherwise:
+    selectedElements = []
+    for number in list(map(lambda x: int(x), userInput)):
+        selectedElements.append(elementList[number-1])
+    return selectedElements
+
+
+def printNumberedList(list: List[str]) -> None:
+    for i in range(0, len(list)):
+        print(str(i + 1) + "-" + list[i])
+
+
+def isNumber(string : str) -> bool:
+    try:
+        float(string)
+        return True
+    except ValueError:
+        return False
 
 
 def checkFrequency1000GAll(x, thr: float) -> bool:
@@ -92,20 +140,58 @@ soup = BeautifulSoup(html_content, "html.parser")
 
 ## SEARCH THE VARIANTS
 
+print('Loading file...')
 df = pd.read_excel(file_path)
+print('Completed.')
 
 ### DATASET FILTERS
 
-df = df.loc[df['Func.refGene'].isin(['exonic', 'exonic;splicing', 'splicing'])]
-df = df.loc[~ df['ExonicFunc.refGene'].isin(['synonymous SNV', 'unknown'])]
+# filter 1
+print("Which mutations do you want to analyze?")
+print('Write your input as (example) 1,2,5,12 (with no space around commas)')
+optionInputMutations = df['Func.refGene'].unique()  # options to select
+printNumberedList(optionInputMutations)
+selectedMutations = getUserInputListFromNumberedList(optionInputMutations)
+printReceivedInput(selectedMutations)
+df = df.loc[df['Func.refGene'].isin(selectedMutations)]
+
+# filter 2
+print("Genes to exclude:")
+print("Write the list here(as gene1,gene2,gene3) with no space around commas")
+print("Press Enter to include all.")
+genesToExclude = getUserInputList()
+printReceivedInput(genesToExclude)
+df = df[~df['Gene.refGene'].str.startswith(tuple(genesToExclude))]
+
+# filter 3
+print('Causative effects to exclude:')
+optionInputCausEff = df['ExonicFunc.refGene'].dropna().unique()  # options to select (removing emtpy cells from options)
+# if there are options to select, ask what to select and then apply filter, do not even ask otherwise
+if len(optionInputCausEff) > 0:
+    print("Write the list here (like 2,3,14) with no space around commas")
+    print("Press Enter to include all.")
+    printNumberedList(optionInputCausEff)
+    causEffToExclude = getUserInputListFromNumberedList(optionInputCausEff)
+    printReceivedInput(causEffToExclude)
+    df = df.loc[~ df['ExonicFunc.refGene'].isin(causEffToExclude)]
+else:
+    print('No selectable options for this filter (considering the filters selected above).\n')
+
+# filter 4
+print('Cutoff:')
+print('(example: to set cutoff to 15%, insert 0.15 or 0,15)')
+selectedCutOff = getUserInput()
+selectedCutOff = selectedCutOff.replace(',', '.')
+if not isNumber(selectedCutOff):
+    raise Exception('The cut off is not a valid number')
+selectedCutOff = float(selectedCutOff) # convert string in actual number
+printReceivedInput(selectedCutOff)
 # generate the column of true values for the values to be filtered
-df['filtro1000G'] = df['1000G_ALL'].apply(lambda x: checkFrequency1000GAll(x,0.02))
+df['filtro1000G'] = df['1000G_ALL'].apply(lambda x: checkFrequency1000GAll(x, selectedCutOff))
 # update dataframe
 df=df[df['filtro1000G']==True]
 # drop undesired column
 df.drop('filtro1000G',axis=1,inplace=True)
-# remove 'HLA' and 'MUC' genes 
-df = df[~df['Gene.refGene'].str.startswith(('HLA', 'MUC'))]
 
 
 # extract raw file name fo the  input file
